70e6485840c71122494fbc1bcd185369642fd8a6
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-08 08:54:08 -0700

    feat: Completed anatomy of a program tutorial

--- Projects/Intro_2_CSharp/Program.cs
+++ Projects/Intro_2_CSharp/Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
+            // a field is a class member (aka variable)
             Actor joleneBlalock = new Actor();
 
-            joleneBlalock.lines = "That's not logical.";
+            Robot hoverBot = new Robot();
+
+            string actorName = "Jolene Blalock";
+            joleneBlalock.lines = "Delightful, a new programming rube.\nVery well.\n";
             joleneBlalock.role = "Provocative Vulcanness";
 
-            Console.WriteLine(joleneBlalock.SayLines());
-            Console.WriteLine($"My role in this irrational development is {joleneBlalock.role}.");
+            Console.WriteLine($"{joleneBlalock.SayLines()}My name is {actorName}");
+            Console.WriteLine($"My role in this irrational development is {joleneBlalock.role}.\nDo you have some witty repartee, hmmm?");
+            string response = Console.ReadLine();
+            Console.WriteLine($"Your response, albeit irrelevant in the grand scheme of things, is . . . '{response}'.\n");
+            hoverBot.Introduction();
         }
     }
 }
---
+++ Projects/Intro_2_CSharp/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_2_CSharp
+{
+    internal class Robot
+    {
+        // a field or variable
+        private string robotType = "hover bot";
+
+        // method
+        public void Introduction()
+        {
+            Console.WriteLine("Beep, boop, beep.\nI'm a {0} type of robot.", robotType);
+        }
+    }
+}
