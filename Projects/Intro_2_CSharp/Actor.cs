9cb986dc88a4fb05ed48d302dc01aeaa801aa9cc
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-08 08:25:04 -0700

    feat: Coded intro metaphor of Actor to class for Object Oriented programming

---
+++ Projects/Intro_2_CSharp/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_2_CSharp
+{
+    public class Actor
+    {
+        public string lines;
+        public string role;
+
+       
+        public string SayLines() 
+        { 
+            return lines; 
+        }
+    }
+
+}
---
+++ Projects/Intro_2_CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Intro_2_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Actor joleneBlalock = new Actor();
+
+            joleneBlalock.lines = "That's not logical.";
+            joleneBlalock.role = "Provocative Vulcanness";
+
+            Console.WriteLine(joleneBlalock.SayLines());
+            Console.WriteLine($"My role in this irrational development is {joleneBlalock.role}.");
+        }
+    }
+}
